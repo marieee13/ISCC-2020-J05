aa9de36616c5a97e39fe5e8071373e25d039c5b3
marieee13 <67273177+marieee13@users.noreply.github.com>
2020-06-29 10:45:26 +0200

    Add files via upload

---
+++ hello-word.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+    <html lang="fr">
+    
+    <head>
+        <title> Hello World </title>
+        <meta charset="utf-8">
+    </head> 
+    <body>
+        <?php 
+echo: "Hello World!";
+?>
+</body>
+</html>
