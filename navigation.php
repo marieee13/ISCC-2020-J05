bfc152fce2644c0e450e3ce859bdb502ad15bdf7
marieee13 <67273177+marieee13@users.noreply.github.com>
2020-06-29 14:26:59 +0200

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>nav</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style/vitrine.css">
+</head>
+<body>
+<nav>
+
+      <a href="vitrine-accueil.php">Accueil</a>
+     <a href="vitrine-contacts.php">Contacts</a>
+     <a href="vitrine-programme.php">Programme</a>
+    
+  
+</nav>
+</body>
+</html>
